7d934f6b99d79c38a5930a611ba9164761a91bd4
vicente <vitor.vicente2023@gmail.com>
2023-09-05 13:55:31 -0300

    DOCS: First commit

---
+++ src/lib/index.js
@@ -0,0 +1,29 @@
+import Bold from './components/Bold';
+import Anchor from './components/Anchor';
+import HelpItem from './components/HelpItem';
+import Row from './components/Row';
+import ButtonWrapper from './components/ButtonWrapper';
+import Label from './components/Label';
+import Labeled from './components/Labeled';
+import ToggleSwitchButton from './components/ToggleSwitchButton';
+import Status from './components/Status';
+import SelectSection from './components/SelectSection';
+import Button from './components/Button';
+import LinkButton from './components/LinkButton';
+import IconButton from './components/IconButton';
+
+export {
+  Bold,
+  Anchor,
+  HelpItem,
+  ButtonWrapper,
+  Row,
+  Label,
+  Labeled,
+  ToggleSwitchButton,
+  Status,
+  SelectSection,
+  Button,
+  LinkButton,
+  IconButton,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const colors = require('tailwindcss/colors');
+
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.js', './src/**/*.jsx', './dist/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+        primary: {
+          ...colors.blue,
+          1000: '#14275c', // 900 but with 1/3 of brightness (value in HSV)
+        },
+        secondary: colors.teal,
+      },
+      fontFamily: {
+        sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        sweep: 'transition 1s ease-in-out',
+      },
+    },
+  },
+
+  plugins: [require('@tailwindcss/forms'), require('autoprefixer')],
+};
